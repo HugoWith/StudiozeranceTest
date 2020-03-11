0e9dc59c5333caf3ec1fef8f8eafa937d07127d3
Hugo Valla <Hugo_valla@hotmail.fr>
2020-03-11 16:12:53 +0100

    Navbar done + Section2 (main title)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <template>
   <div id="app">
+    <Navbar />
+    <Section2 />
     <router-view />
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar";
+import Section2 from "@/components/Section2";
+import Section3 from "@/components/Section3";
+
+export default {
+  name: "app",
+  components: {
+    Navbar,
+    Section2,
+    Section3
+  }
+};
+</script>
 
 <style lang="scss">
 $maincolor: #ffcd1a;
---
+++ src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="navbar">
+    <div class="block-left">
+      <!-- <img src="@/assets/Navbar/logo.png" alt="logoZerance" /> -->
+      <div class="img"></div>
+      <h2>
+        Studio
+        <br />Zerance.
+      </h2>
+    </div>
+    <div class="block-right">
+      <h2>Nos Clients</h2>
+      <h2>Notre Offre</h2>
+      <h2>Le Studio</h2>
+      <h2>Contact</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  height: 20vh;
+}
+
+.img {
+  background-image: url("../assets/Navbar/logo.png");
+  width: 125px;
+  height: 650px;
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+
+.block-left {
+  display: flex;
+  padding: 10px;
+
+  h2 {
+    margin-left: 10px;
+  }
+}
+.block-right {
+  display: flex;
+  justify-content: space-evenly;
+  h2 {
+    margin-left: 40px;
+  }
+}
+</style>
